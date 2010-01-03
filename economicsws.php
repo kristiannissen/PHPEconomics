e8280ef57f888a53c630c9dabd73cd00617d08da
= <=>
2010-01-03 09:05:37 +0100

    Updated version, see wiki

--- economicsws.php
+++ economicsws.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author: Kristian Nissen
- * @version: 0.2
+ * @version: 0.4
  */
 class EconomicsWS {
   // Economics SOAP
@@ -77,8 +77,9 @@ class EconomicsWS {
           'Address' => $params['debitor_address'],
           'PostalCode' => $params['debitor_postalcode'],
           'City' => $params['debitor_city'],
-          'Country' => $params['debitor_city'],
-          'OurReferenceHandle' => $employee_handle
+          'Country' => $params['debitor_country'],
+          'OurReferenceHandle' => $employee_handle,
+          'CINumber' => $params['debitor_cvr']
         );        
 		    $this->client->Debtor_CreateFromData(array(
 		      'data' => $debtor_data
@@ -108,12 +109,235 @@ class EconomicsWS {
 	    }
 	  } catch (Exception $e) {
       syslog(LOG_DEBUG, 'exception '. $e->getMessage() .' at line '. $e->getLine() .' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
-      $number = 0;
+      return 0;
 	  }
-	  syslog(LOG_DEBUG, 'new debtor '. $number);
 
 	  return intval($number);
-  }  
+  }
+  /**
+   * @param mixed $params
+   * @return boolean
+   */
+  function update_debitor($params, $vat_zone = 'HomeCountry') {
+    try {
+      $debtor = $this->client->Debtor_FindByNumber(array(
+        'number' => $params['debitor_number']
+      ))->Debtor_FindByNumberResult;
+	    
+	    $debtor_group_handles = $this->client->debtorGroup_GetAll()->DebtorGroup_GetAllResult->DebtorGroupHandle;
+	    
+	    $currency_handle = $this->client->Currency_FindByCode(array(
+        'code' => $params['currency']
+      ))->Currency_FindByCodeResult;
+      
+      $term_handle = $this->client->TermOfPayment_FindByName(array(
+        'name' => $params['term_of_payment']
+      ))->TermOfPayment_FindByNameResult->TermOfPaymentHandle;
+      
+      $templatecollection_handle = $this->client->TemplateCollection_FindByName(array(
+        'name' => $params['template_name']
+      ))->TemplateCollection_FindByNameResult->TemplateCollectionHandle;
+	    
+	    $debtor_handle = $this->client->Debtor_UpdateFromData(array(
+	      'data' => (object) array(
+	        'Handle' => $debtor,
+	        'Number' => $debtor->Number,
+	        'VatZone' => $vat_zone,
+	        'IsAccessible' => true,
+	        'Name' => $params['debitor_name'],
+          'Email' => $params['debitor_email'],
+          'CINumber' => $params['debitor_vatnumber'],
+          'TelephoneAndFaxNumber' => $params['debitor_phone_and_fax'],
+          'Website' => $params['debitor_website'],
+          'Address' => $params['debitor_address'],
+          'PostalCode' => $params['debitor_postalcode'],
+          'City' => $params['debitor_city'],
+          'Country' => $params['debitor_country'],
+          'DebtorGroupHandle' => $debtor_group_handles,
+          'CurrencyHandle' => $currency_handle,
+          'TermOfPaymentHandle' => $term_handle,
+          'LayoutHandle' => $templatecollection_handle
+	      )
+	    ))->Debtor_UpdateFromDataResult;
+	    
+	    if (!empty($params['debitor_ean'])) {
+        $this->client->Debtor_SetEan(array(
+          'debtorHandle' => $debtor,
+          'valueHandle' => $params['debitor_ean']
+        ));      
+      }
+	    
+    } catch (Exception $e) {
+      syslog(LOG_DEBUG, ' SOAP response '. $this->client->__getLastResponse());
+      return false;
+    }
+    
+    return true;
+  }
+  /**
+   * @param mixed $params
+   * @return boolean
+   */
+  function add_order_to_invoice($params) {
+    try {
+      $debtor_current_invoices = $this->client->Debtor_GetCurrentInvoices(array(
+        'debtorHandle' => (object)array(
+          'Number' => $params['debitor_number']
+        )
+      ))->Debtor_GetCurrentInvoicesResult->CurrentInvoiceHandle;
+
+      if(gettype($debtor_current_invoices) == 'array'){
+        $current_invoice = array_shift($debtor_current_invoices);
+      } else if(gettype($debtor_current_invoices) == 'object'){
+        $current_invoice = $debtor_current_invoices;
+      } else {
+        $current_invoice = $this->client->CurrentInvoice_Create(array(
+          'debtorHandle' => (object)array(
+            'Number' => $params['debitor_number']
+          ) 
+        ))->CurrentInvoice_CreateResult;
+      }
+
+      $invoiceline_handle = $this->client->CurrentInvoiceLine_Create(array(
+        'invoiceHandle' => $current_invoice
+      ))->CurrentInvoiceLine_CreateResult;
+
+      $product_handle = $this->client->Product_FindByNumber(array(
+        'number' => $params['product_number']
+      ))->Product_FindByNumberResult;
+      
+      $product_data_handle = $this->client->Product_GetData(array(
+        'entityHandle' => $product_handle
+      ))->Product_GetDataResult;
+      
+      $current_invoiceline_handle = $this->client->CurrentInvoiceLine_CreateFromData(array(
+        'data' => array(
+          'Handle' => $invoiceline_handle,
+          'Id' => $current_invoice->Id,
+          'Number' => $invoiceline_handle->Number,
+          'InvoiceHandle' => $current_invoice,
+          'Description' => empty($params['product_description']) ? $product_data_handle->Name : $params['product_description'],
+          'DeliveryDate' => NULL,
+          'ProductHandle' => $product_handle,
+          'Quantity' => floatval($params['product_quantity']),
+          'UnitNetPrice' => $product_data_handle->SalesPrice,
+          'DiscountAsPercent' => floatval($params['product_discount']),          
+          'UnitCostPrice' => 0.0,
+          'TotalNetAmount' => (floatval($product_data_handle->SalesPrice) * floatval($params['product_quantity'])),
+          'TotalMargin' => 0.0,
+          'MarginAsPercent' => 0.0
+        )
+      ))->CurrentInvoiceLine_CreateFromDataResult;
+      
+    } catch (Exception $e) {
+      syslog(LOG_DEBUG, 'exception '. $e->getMessage() .' at line '. $e->getLine() .' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
+      return false;
+    }
+    return true;
+  }
+  /**
+   * @return mixed
+  */
+  function get_product_groups() {
+    return $this->client->ProductGroup_GetAll()->ProductGroup_GetAllResult;
+  }
+  /**
+   * @return mixed
+   */
+  function get_products() {
+    return $this->client->Product_GetAll()->Product_GetAllResult->ProductHandle;
+  }
+  /**
+   * @params int debitor id
+   * @return boolean
+   */
+  function add_subscriber($params) {
+    try {
+      $subscriber_handle = $this->client->Subscriber_Create(array(
+        'debtorHandle' => (object)array(
+          'Number' => $params['debitor_number']
+        ),
+        'startDate' => $params['start_date'],
+        'endDate' => $params['end_date'],
+        'registeredDate' => date('Y-m-d'). 'T00:00:00',
+        'subscriptionHandle' => (object)array(
+          'Id' => intval($params['subscription_id'])
+        )
+      ))->Subscriber_CreateResult;
+    } catch (Exception $e) {
+      syslog(LOG_DEBUG, 'exception '. $e->getMessage() .' at line '. $e->getLine() .' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
+      return false;
+    }
+    
+    return true;
+  }
+  /**
+   * @params mixed
+   * @return boolean
+   */
+  function expire_subscriber($params) {
+    try {                  
+      $this->client->Subscriber_SetExpiryDate(array(
+        'subscriberHandle' => (object) array(
+          'SubscriberId' => $params['subscriber_id']
+        ),
+        'value' => $params['expire_date']
+      ));
+      
+    } catch (Exception $e) {
+      //syslog(LOG_DEBUG, ' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
+      return false;
+    }
+    
+    return true;
+  }
+  /**
+   * @return mixed
+   */
+  function get_subscribers() {
+    return $this->client->Subscriber_GetAll()->Subscriber_GetAllResult->SubscriberHandle;
+  }
+  /**
+   * @return mixed
+   */
+  function get_subscriptions() {
+    return $this->client->Subscription_GetAll()->Subscription_GetAllResult->SubscriptionHandle;
+  }
+  /**
+   * @param string debitor number
+   * @return boolean
+   */
+  function download_current_invoice($params, $format = 'pdf') {
+    try {      
+      $invoice_handle = $this->client->CurrentInvoice_Book(array(
+        'currentInvoiceHandle' => (object)array(
+          'Id' => intval($params['current_invoice_id'])
+        )
+      ))->CurrentInvoice_BookResult;
+      
+      $invoice = "";
+      
+      switch ($format) {
+        default:
+        case 'pdf':
+          $invoice = $this->client->Invoice_GetPdf(array(
+            'invoiceHandle' => $invoice_handle
+          ))->Invoice_GetPdfResult;
+            break;
+        case 'oioxml':
+          $invoice = $this->client->Invoice_GetOioXml(array(
+            'invoiceHandle' => $invoice_handle
+          ))->Invoice_GetOioXmlResult;
+            break;
+      }
+      
+    } catch (Exception $e) {
+      syslog(LOG_DEBUG, 'exception '. $e->getMessage() .' at line '. $e->getLine() .' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
+      return false;
+    }
+    
+    return $invoice;
+  }
   /**
    * @return mixed
    */
@@ -161,6 +385,25 @@ class EconomicsWS {
     return $employees;
   }
   /**
+   * @return mixed
+   */
+  function get_debitor_current_invoices($params) {
+    try {
+      $debtor = $this->client->Debtor_FindByNumber(array(
+	      'number' => $params['debitor_number']
+	    ))->Debtor_FindByNumberResult;
+	    
+      $current_invoices = $this->client->Debtor_GetCurrentInvoices(array(
+        'debtorHandle' => $debtor
+      ))->Debtor_GetCurrentInvoicesResult->CurrentInvoiceHandle;
+    } catch (Exception $e) {
+      syslog(LOG_DEBUG, 'exception '. $e->getMessage() .' at line '. $e->getLine() .' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
+      $current_invoices = array();
+    }
+    
+    return $current_invoices;
+  }
+  /**
    * @return object
    */
   function get_debitor($params) {
@@ -178,5 +421,25 @@ class EconomicsWS {
     }
     
     return $debtor_data;
+  }
+  /**
+   * @return mixed
+   */
+  function get_debitor_invoices($params) {
+    try {
+      $debtor = $this->client->Debtor_FindByNumber(array(
+        'number' => $params['debitor_number']
+      ))->Debtor_FindByNumberResult;
+      
+      $invoices = $this->client->Debtor_GetInvoices(array(
+        'debtorHandle' => $debtor
+      ))->Debtor_GetInvoicesResult->InvoiceHandle;
+
+    } catch (Exception $e) {
+      syslog(LOG_DEBUG, 'exception '. $e->getMessage() .' at line '. $e->getLine() .' SOAP request '. $this->client->__getLastRequest() .' SOAP response '. $this->client->__getLastResponse());
+      $invoices = array();
+    }
+    
+    return $invoices;
   }
 }
--- tests/DescribeDebtor.php
+++ tests/DescribeDebtor.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @author: Kristian Nissen
- * @version: 0.1
+ * 
  */
-include '../economicsws.php';
- 
+include realpath(dirname(__FILE__) .'/../economicsws.php');
+
 class DescribeDebtor extends PHPSpec_Context {
   protected $econ;  
   
@@ -16,27 +16,56 @@ class DescribeDebtor extends PHPSpec_Context {
     
     unset($this->econ);
   }
-  /**   
-   */
   function itShouldReturnIdOnCreate() {
     $params = array(		
-		  'debitor_name' => '',
+		  'debitor_name' => 'CompuGlobalHyperMegaNet',
 		  'debitor_ean' => '',
-		  'debitor_email' => '',
-		  'debitor_phone_and_fax' => '',
-		  'debitor_website' => '',
-		  'debitor_address' => '',
-		  'debitor_postalcode' => '',
-		  'debitor_city' => '',
-		  'debitor_country' => '',
-		  'debitor_vatnumber' => '',
-		  'currency' => '',
-		  'term_of_payment' => '',
-		  'template_name' => '',
-		  'debitor_attention' => '',
-		  'employee_number' => ''
+		  'debitor_email' => 'chunkylover53@aol.com',
+		  'debitor_phone_and_fax' => '80808080',
+		  'debitor_website' => 'http://www.example.com',
+		  'debitor_address' => 'et eller andet sted',
+		  'debitor_postalcode' => '1100',
+		  'debitor_city' => 'en eller anden gade',
+		  'debitor_country' => 'Danmark',
+		  'debitor_cvr' => '123456789',
+		  'currency' => 'DKK',
+		  'term_of_payment' => 'Netto 8 dage',
+		  'template_name' => '22.05.07',
+		  'debitor_attention' => 'Homer Simpson',
+		  'employee_number' => '007'
 	  );	
 	  $this->spec($this->econ->create_debitor($params) > 0)->should->beTrue();	
+  } 
+  function itShouldReturnTrueWhenNewOrderIsAddedToInvoice() {
+	  $params = array(
+	    'debitor_number' => '78263',
+	    'product_number' => '200',
+	    'product_quantity' => rand(1,3),
+	    'product_description' => ''
+	  );	  
+	  $this->spec($this->econ->add_order_to_invoice($params))->should->beTrue();
+  }
+  function itShouldReturnStringWhenCurrentInvoiceIsDownloaded() {
+    $params = array(
+      'current_invoice_id' => '917'
+    );    
+    $this->spec(gettype($this->econ->download_current_invoice($params)) == 'string')->should->beTrue();
+  }
+  function itShouldReturnTrueWhenDebtorIsAddedToRecurringPayments() {
+    $params = array(
+      'debitor_number' => '78184',
+      'start_date' => '2008-07-24T00:00:00',
+      'end_date' => '2008-08-24T00:00:00',
+      'subscription_id' => '1005'
+    );    
+    $this->spec($this->econ->add_subscriber($params))->should->beTrue();
+  }
+  function itShouldReturnTrueWhenExpireSubscriberIsCalled() {
+    $params = array(
+      'subscriber_id' => '191',
+      'expire_date' => '2009-08-18T00:00:00'
+    );
+    $this->spec($this->econ->expire_subscriber($params))->should->beTrue();
   }
   /**
    * Helpers
@@ -49,23 +78,57 @@ class DescribeDebtor extends PHPSpec_Context {
   }
   function itShouldReturnAnArrayWhenGetCurrenciesIsCalled() {
     $this->spec(gettype($this->econ->get_currencies()) == 'array')->should->beTrue();
-  }  
+  }
+  function itShouldReturnAnArrayWhenGetSubscriptionsIsCalled() {
+    $this->spec(gettype($this->econ->get_subscriptions()) == 'array')->should->beTrue();
+  }
+  function itShouldReturnAnArrayWhenGetSubscribersIsCalled() {
+    $this->spec(gettype($this->econ->get_subscribers()) == 'array')->should->beTrue();    
+  }
+  function itShouldReturnAnArrayWhenGetProductsIsCalled() {
+    $this->spec(gettype($this->econ->get_products()) == 'array')->should->beTrue();
+  }
   function itShouldReturnAnArrayWhenGetDebtorsIsCalled() {
     $this->spec(gettype($this->econ->get_debitors()) == 'array')->should->beTrue();
   }
   function itShouldReturnAnArrayWhenGetEmployessIsCalled() {
     $this->spec(gettype($this->econ->get_employees()) == 'array')->should->beTrue();
-  }  
+  }
+  function itShouldReturnAnArrayWhenGetDebitorCurrentInvoicesIsCalled() {
+    $params = array(
+      'debitor_number' => '78263'
+    );
+    $this->spec(gettype($this->econ->get_debitor_current_invoices($params)) == 'array')->should->beTrue();
+  }
   function itShouldReturnAnObjectWhenGetDebitorIsCalled() {
     $params = array(
-      'debitor_number' => ''
+      'debitor_number' => '78263'
     );
     $this->spec(gettype($this->econ->get_debitor($params)) == 'object')->should->beTrue();
   }
   function itShouldReturnAnArrayWhenGetDebitorInvoicesIsCalled() {
     $params = array(
-      'debitor_number' => ''
+      'debitor_number' => '78263'
     );
     $this->spec(count($this->econ->get_debitor_invoices($params)) > 0)->should->beTrue();
+  }
+  function itShouldReturnTrueWhenDebitorIsUpdated() {
+    $params = array(
+      'debitor_number' => '78263',
+      'debitor_name' => 'CompuGlobalHyperMegaNet',
+		  'debitor_ean' => '',
+		  'debitor_email' => 'chunkylover53@aol.com',
+		  'debitor_phone_and_fax' => '80808080',
+		  'debitor_website' => 'http://www.example.co.uk',
+		  'debitor_address' => '742 Evergreen Terrace',
+		  'debitor_postalcode' => '1100',
+		  'debitor_city' => 'Springfield',
+		  'debitor_country' => 'Danmark',
+		  'debitor_vatnumber' => '26901626',
+		  'currency' => 'DKK',
+		  'term_of_payment' => 'Netto 8 dage',
+		  'template_name' => '22.05.07'
+    );
+    $this->spec($this->econ->update_debitor($params))->should->beTrue();
   }
 }
