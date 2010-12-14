e575c9d2c783aab2c07362a4708d7d1b9ed50af6
Kristian Nissen <kristiannissen@Kristian-Nissens-MacBook-Pro.local>
2010-12-14 20:32:54 +0100

    Committing tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Establish reference to simpletest
+ * we will include this in all our tests
+ * it bootstraps the test suite
+ */
+
+$path_to_simpletest = realpath(dirname(dirname(dirname(__FILE__))));
+
+set_include_path(join(PATH_SEPARATOR, array($path_to_simpletest, get_include_path())));
+
+require_once 'simpletest/autorun.php';
+
+require_once 'PHPEconomics/economicsws.php';
---
+++ tests/debtortest.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Run test for debtor
+ */
+
+include_once 'bootstrap.php';
+
+class TestDebtor extends UnitTestCase {
+	protected $ew;
+	
+	function setUp () {
+		$this->ew = new EconomicsWS;
+	}
+	
+	function tearDown () {
+		unset($this->ew);
+	}
+	
+	function testGetDebtors () {
+		// Get list of all debtors
+		$debtors = $this->ew->get_debtors();
+		// Test that we have a list of debtors
+		$this->assertTrue(count($debtors) > 0);
+	}
+	/*
+	 * Returns a debtor data object for a given debtor.
+	 * See https://www.e-conomic.com/secure/api1/EconomicWebservice.asmx?op=Debtor_GetData for more details
+	 */
+	function testGetDebtor () {
+		// We will just take the first debtor for testing
+		$debtor = array_shift($this->ew->get_debtors());
+		// Test that our debtor has a handle
+		$this->assertTrue(property_exists($debtor, 'Handle'));
+		// Make sure number is an int
+		$number = intval($debtor->Handle->Number);
+		// Fetch debtor
+		$debtor = $this->ew->get_debtor(array('number' => $number));
+		// Verify that our debtor has a name
+		$this->assertTrue(property_exists($debtor, 'Name'));
+	}
+	/*
+	 * get_debtor_invoices
+	 * https://www.e-conomic.com/secure/api1/EconomicWebservice.asmx?op=Invoice_GetDataArray
+	 */
+	function testGetDebtorInvoices () {
+		// We will just take the first debtor for testing
+		$debtor = array_shift($this->ew->get_debtors());
+		// Test that our debtor has a handle
+		$this->assertTrue(property_exists($debtor, 'Handle'));
+		// Make sure number is an int
+		$number = intval($debtor->Handle->Number);
+		// Get list of invoices
+		$invoices = $this->ew->get_debtor_invoices(array('number' => $number));
+		// Test that we have a valid response
+		$this->assertTrue(gettype($invoices) == 'array');
+	}
+}
