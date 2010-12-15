382aa750edec3f82292197d42a8015cd909673ad
unknown <bdi-krni@.root.local>
2010-12-15 08:47:21 +0100

    Error logging

--- economicsws.php
+++ economicsws.php
@@ -4,6 +4,8 @@
  * @version: 0.4
  */
 
+require_once realpath(join(PATH_SEPARATOR, array(dirname(__FILE__), 'economicsexception.php'))));
+
 class EconomicsWS {
   // Soap client
   private $client;
