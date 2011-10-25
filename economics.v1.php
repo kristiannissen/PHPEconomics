24974094d3b82b123523c5063c4fee1e6279e222
Kristian Nissen <kristian.nissen@gmail.com>
2011-10-25 11:44:49 +0200

    more features

--- economics.v1.php
+++ economics.v1.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ *
+ */
 
 function get_soap_connection() {
 	static $client;
@@ -20,6 +23,118 @@ function get_soap_connection() {
 	}
 	
 	return $client;
+}
+
+/**
+ * Example:
+ * if ($debtor == debtor_find_by_name('CompuGlobalHyperMegaNet')) {
+ *	# Do something with debtor
+ * } else {
+ *	# No debtor found by that name
+ * }
+ */
+function debtor_find_by_name($name = null) {
+	global $soap_client;
+	
+	if (!is_string($name)) {
+		trigger_error("Debtor name must be a string", E_USER_ERROR);
+	}
+	
+	$result = $soap_client->Debtor_FindByName(array(
+		'name' => $name
+	));
+	
+	if (is_object($result) && property_exists($result, 'Debtor_FindByNameResult')) {
+		if (property_exists($result->Debtor_FindByNameResult, 'DebtorHandle')) {
+			$handle = $result->Debtor_FindByNameResult->DebtorHandle;
+			
+			if (function_exists('debtor_find_by_number')) {
+				return debtor_find_by_number(intval($handle->Number));
+			}
+			
+			return $handle;
+		}
+	}
+	
+	return null;
+}
+
+function debtor_geocode_address($debtor = null) {
+	global $soap_client;
+	
+	if (!function_exists('json_decode')) {
+		trigger_error("This feature required json_decode() see http://dk2.php.net/manual/en/function.json-decode.php", E_USER_ERROR);
+	}
+	// http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true_or_false
+	
+	$address = array(
+		urlencode($debtor->Address),
+		urlencode($debtor->PostalCode),
+		urlencode($debtor->City),
+		urlencode($debtor->Country)
+	);
+	
+	if (function_exists('http_get')) {
+		# Use http_get http://dk2.php.net/manual/en/function.http-get.php
+	} else {
+		$google = file_get_contents(sprintf("http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false", join(',', $address)));
+	}
+	
+	$resp = json_decode($google);
+	
+	return $resp->results;
+}
+
+function debtor_uses_twitter($debtor = null) {
+	global $soap_client;
+	
+	if (!function_exists('json_decode')) {
+		trigger_error("This feature required json_decode() see http://dk2.php.net/manual/en/function.json-decode.php", E_USER_ERROR);
+	}
+	
+	if (function_exists('http_get')) {
+		# Use http_get http://dk2.php.net/manual/en/function.http-get.php
+	} else {
+		$twitter = file_get_contents("http://search.twitter.com/search.json?q=%40". urlencode($debtor->Name));
+	}
+	
+	$resp = json_decode($twitter);
+	
+	return $resp->results;
+}
+
+/**
+ * Example:
+ * if ($debtor == debtor_find_by_number(42)) {
+ *	# Do something with debtor
+ * } else {
+ *	# No debtor found
+ * }
+ */
+function debtor_find_by_number($number = null) {
+	global $soap_client;
+	
+	if (!is_int($number)) {
+		trigger_error("Debtor number must be an integer", E_USER_ERROR);
+	}
+	
+	$result = $soap_client->Debtor_FindByNumber(array(
+		'number' => $number
+	));
+
+	if (is_object($result) && property_exists($result, 'Debtor_FindByNumberResult')) {
+		if (property_exists($result->Debtor_FindByNumberResult, 'Number')) {
+			$debtor = $soap_client->Debtor_GetData(array(
+				'entityHandle' => $result->Debtor_FindByNumberResult
+			));
+			
+			if (is_object($debtor) && property_exists($debtor, 'Debtor_GetDataResult')) {
+				return $debtor->Debtor_GetDataResult;
+			}
+		}
+	}
+	
+	return null;
 }
 /**
  * Example:
@@ -51,7 +166,7 @@ function debtor_get_all() {
 
 /**
  * Example:
- * if ($invoices == debtor_get_invoices($params)) {
+ * if ($invoices == debtor_get_invoices(42)) {
  * 	# Do something with invoices
  * } else {
  * 	# No invoices for debtor found
@@ -65,19 +180,57 @@ function debtor_get_invoices($number = null) {
 		trigger_error("debtor_get_invoices('42') takes a debtor number as it's parameter", E_USER_ERROR);
 	}
 	
-	$result = $soap_client->Debtor_GetInvoices(array(
+	$debtor = $soap_client->Debtor_FindByNumber(array(
+    'number' => $number
+  ));
+
+	if (is_object($debtor) && property_exists($debtor, 'Debtor_FindByNumberResult')) {
+		$result = $soap_client->Debtor_GetInvoices(array(
+			'debtorHandle' => $debtor->Debtor_FindByNumberResult
+		));
+
+		if (is_object($result) && property_exists($result, 'Debtor_GetInvoicesResult')) {
+			$invoice_handles = $result->Debtor_GetInvoicesResult->InvoiceHandle;
+
+			$result = $soap_client->Invoice_GetDataArray(array('entityHandles' => $invoice_handles));
+
+			if (is_object($result) && property_exists($result, 'Invoice_GetDataArrayResult')) {
+				return $result->Invoice_GetDataArrayResult->InvoiceData;
+			}
+		}
+	}
+	
+	return null;
+}
+/**
+ * Example:
+ * CurrentInvoice_GetDataArray
+ */
+function debtor_get_current_invoices($number = null) {
+	global $soap_client;
+	
+	// Make sure we have a valid number
+	if (is_null($number)) {
+		trigger_error("debtor_get_current_invoices('42') takes a debtor number as it's parameter", E_USER_ERROR);
+	}
+	
+	$result = $soap_client->Debtor_GetCurrentInvoices(array(
 		'debtorHandle' => (object) array(
 			'Number' => (string) $number
 		)
 	));
-	
-	if (is_object($result) && property_exists($result, 'Debtor_GetInvoicesResult')) {
-		$invoice_handles = $result->Debtor_GetInvoicesResult->InvoiceHandle;
-		
-		$result = $soap_client->Invoice_GetDataArray(array('entityHandles' => $invoice_handles));
-		
-		if (is_object($result) && property_exists($result, 'Invoice_GetDataArrayResult')) {
-			return $result->Invoice_GetDataArrayResult->InvoiceData;
+
+	if (is_object($result) && property_exists($result, 'Debtor_GetCurrentInvoicesResult')) {
+		if (property_exists($result->Debtor_GetCurrentInvoicesResult, 'CurrentInvoiceHandle')) {
+			$handles = $result->Debtor_GetCurrentInvoicesResult->CurrentInvoiceHandle;
+			
+			$result = $soap_client->CurrentInvoice_GetDataArray(array(
+				'entityHandles' => $handles
+			));
+
+			if (property_exists($result, 'CurrentInvoice_GetDataArrayResult')) {
+				return $result->CurrentInvoice_GetDataArrayResult->CurrentInvoiceData;
+			}
 		}
 	}
 	
--- tests/economicstest.php
+++ tests/economicstest.php
@@ -22,20 +22,76 @@ class EconomicsTest extends UnitTestCase
   function tearDown() {}
 	// SoapClient
 	function testSoapClient() {}
+	
+	function testDebtorNotFoundByName() {
+		$debtor = debtor_find_by_name('CompuGlobalHyperMegaNet');
+		
+		$this->assertTrue(is_null($debtor));
+	}
+
+	function testDebtorFindByName() {
+		$debtor = debtor_find_by_name('Expotium GmbH');
+
+		$this->assertTrue(is_object($debtor));
+	}
+	
+	function testDebtorFindOnTwitter() {
+		$debtor = debtor_find_by_name('Expotium GmbH');
+		$twitter = debtor_uses_twitter($debtor);
+		
+		$this->assertTrue(is_array($twitter));
+	}
+	
+	function testDebtorGeocodeAddress() {
+		$debtor = debtor_find_by_name('Expotium GmbH');
+		$geocode = debtor_geocode_address($debtor);
+		
+		$this->assertTrue(is_array($geocode));
+	}
+	
+	function testDebtorFindByNumber() {
+		$debtor = debtor_find_by_number(107);
+		
+		$this->assertTrue(is_object($debtor));
+	}
+	
+	function testDebtorNotFoundByNumber() {
+		$debtor = debtor_find_by_number(42);
+		
+		$this->assertTrue(is_null($debtor));
+	}
+
 	// Debtor_GetAll
   function testDebtorGetAll() {
-		$soap_client = get_soap_connection();
-		
 		$debtors = debtor_get_all();
-		
+
 		$this->assertTrue(is_array($debtors));
   }
 	// Debtor_GetData
 	function testDebtorGetInvoices() {
-		$soap_client = get_soap_connection();
 		
 		$invoices = debtor_get_invoices(105);
 		
 		$this->assertTrue(is_array($invoices));
+	}
+	
+	function testDebtorHasNoInvoices() {
+		$invoices = debtor_get_invoices(42);
+		
+		$this->assertTrue(is_null($invoices));
+	}
+	
+	// Debtor_GetData
+	function testDebtorGetCurrentInvoices() {
+		
+		$invoices = debtor_get_current_invoices(107);
+		
+		$this->assertTrue(is_array($invoices));
+	}
+	
+	function testDebtorHasNoCurrentInvoices() {
+		$invoices = debtor_get_current_invoices(105);
+		
+		$this->assertTrue(is_null($invoices));
 	}
 }
